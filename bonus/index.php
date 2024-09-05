173d9eb8f23366324d2e78da5da391880d3d814a
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-05 16:27:41 +0200

    [UPD] fisrt bonus done

--- bonus/index.php
+++ bonus/index.php
@@ -1,44 +1,46 @@
 <?php
 
-$hotels = [
+    $hotels = [
 
-    [
-        'name' => 'Hotel Belvedere',
-        'description' => 'Hotel Belvedere Descrizione',
-        'parking' => true,
-        'vote' => 4,
-        'distance_to_center' => 10.4
-    ],
-    [
-        'name' => 'Hotel Futuro',
-        'description' => 'Hotel Futuro Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 2
-    ],
-    [
-        'name' => 'Hotel Rivamare',
-        'description' => 'Hotel Rivamare Descrizione',
-        'parking' => false,
-        'vote' => 1,
-        'distance_to_center' => 1
-    ],
-    [
-        'name' => 'Hotel Bellavista',
-        'description' => 'Hotel Bellavista Descrizione',
-        'parking' => false,
-        'vote' => 5,
-        'distance_to_center' => 5.5
-    ],
-    [
-        'name' => 'Hotel Milano',
-        'description' => 'Hotel Milano Descrizione',
-        'parking' => true,
-        'vote' => 2,
-        'distance_to_center' => 50
-    ],
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
 
-];
+    ];
+
+    $parking_filter = isset($_GET['parking'])? $_GET['parking'] : null;
 
 ?>
 
@@ -56,8 +58,23 @@ $hotels = [
 
 <body>
     <h1 class="text-center py-5">Boolean Hotels List</h1>
-
     <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="../bonus/index.php" method="GET" >
+                    <label class="fs-3" for="parking">Parcheggio</label>
+                    <select name="parking" id="parking" class="form-select w-25" onchange="this.form.submit()">
+                        <option value="">Scegli una delle opzioni</option>
+                        <option value="true" <?php if ($parking_filter === 'true') echo 'selected' ?>>Yes</option>
+                        <option value="false" <?php if ($parking_filter === 'false') echo 'selected'?>>No</option>
+                    </select>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container py-5">
         <div class="row">
             <div class="col-12 fs-4">
                 <table class="table border text-center">
@@ -74,13 +91,19 @@ $hotels = [
                     </thead>
                     <tbody>
                         <?php foreach ($hotels as $hotel) {
-                            echo '<tr>';
-                            echo '<td class="text-danger fst-italic fw-bold">' . $hotel['name'] . '</td>';
-                            echo '<td>' . $hotel['description'] . '</td>';
-                            echo '<td>' . ($hotel['parking'] ? 'Yes' : 'No') . '</td>';
-                            echo '<td>' . $hotel['vote'] . '/5 &#9733;' . '</td>';
-                            echo '<td>' . $hotel['distance_to_center'] . ' km' . '</td>';
-                            echo '</tr>';
+                            if (
+                                $parking_filter === '' ||
+                                ($parking_filter === 'true' && $hotel['parking']) ||
+                                ($parking_filter === 'false' && !$hotel['parking'])
+                            ) {
+                                echo '<tr>';
+                                echo '<td class="text-danger fst-italic fw-bold">' . $hotel['name'] . '</td>';
+                                echo '<td>' . $hotel['description'] . '</td>';
+                                echo '<td>' . ($hotel['parking'] ? 'Yes' : 'No') . '</td>';
+                                echo '<td>' . $hotel['vote'] . '/5 &#9733;' . '</td>';
+                                echo '<td>' . $hotel['distance_to_center'] . ' km' . '</td>';
+                                echo '</tr>';
+                            }
                         } ?>
                     </tbody>
                 </table>
